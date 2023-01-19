50f5dd05534b7c8535cfdb754ed79760cfd6f526
serhiibob <sergo.bobr02@gmail.com>
2023-01-19 11:59:37 +0200

    first commit

---
+++ helloWord.js
@@ -0,0 +1,26 @@
+console.log('Hello word!');
+// let a;
+// a = '88';
+// console.log(a + 9);
+// function test() {
+//     console.log("I am Bob");
+// }
+// test();
+
+// const b = () => {
+//     console.log("qwer");
+// }
+
+// b();
+
+
+// let i = 0
+
+// function x() {
+//   i++
+//   return 10
+// }
+
+// i += x()
+
+// console.log(i);
